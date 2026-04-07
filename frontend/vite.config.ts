7bcaab0da2b501e25a7359af0c09f192c542280b
Diego <saherdiego@gmail.com>
2026-04-07 11:02:06 -0600

    Agregamos ssl

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
     host: true,
     port: 5173
